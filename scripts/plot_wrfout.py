e493257cc2bcb03c64d912d8719e32cb7a222124
Igor S. C. <igor.carvalho2@aluno.cps.sp.gov.br>
2026-05-01 11:49:44 -0300

    Setup para plotagem de dados com wrf-python

---
+++ scripts/plot_wrfout.py
@@ -0,0 +1,9 @@
+# Programa para plotar o output do WRF utilizando a biblioteca wrf-pyton
+# Imports ----
+from netCDF4 import Dataset
+import matplotlib.pyplot as plt
+import cartopy.crs as ccrs
+import matplotlib.ticker as mticker
+from cartopy.mpl.gridliner import LONGITUDE_FORMATTER, LATITUDE_FORMATTER
+from mpl_toolkits.axes_grid1 import make_axes_locatable
+from wrf import (getvar, interplevel, to_np, latlon_coords)
